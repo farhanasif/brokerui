81f24b700cfaf659179264fa1285041581a88fd0
Md Farhan Asif <f.asif2010@gmail.com>
2019-08-06 13:33:05 +0600

    order history updates

--- components/reports/OrderHistoryComponent.js
+++ components/reports/OrderHistoryComponent.js
@@ -10,12 +10,12 @@ const item_data = [
     {
         "id": "15",
         "user_id": "147",
-        "client_code": "2209078",
+        "client_code": "ABBANK",
         "name": "MD FARHAN ASIF",
         "mobile": "01716313161",
-        "bank_name": "DUTCH-BANGLA BANK LTD",
+        "bank_name": "44.50",
         "branch_name": "MOTIJHEEL FOREIGN EXCHANGE",
-        "account_no": "105.103.15790",
+        "account_no": "10",
         "created_at": "3rd Apr, 2019 01:45:48 PM",
         "updated_at": "3rd Apr, 2019 01:46:01 PM",
         "status": "1",
@@ -24,54 +24,54 @@ const item_data = [
     {
         "id": "14",
         "user_id": "147",
-        "client_code": "2209078",
+        "client_code": "AAMRANET",
         "name": "MD FARHAN ASIF",
         "mobile": "01716313161",
-        "bank_name": "DUTCH-BANGLA BANK LTD",
+        "bank_name": "25.40",
         "branch_name": "MOTIJHEEL FOREIGN EXCHANGE",
-        "account_no": "105.103.15790",
+        "account_no": "10",
         "created_at": "3rd Apr, 2019 01:43:33 PM",
         "updated_at": "3rd Apr, 2019 01:44:21 PM",
-        "status": "6",
+        "status": "5",
         "amount": "2,000"
     },
     {
         "id": "10",
         "user_id": "147",
-        "client_code": "2209078",
+        "client_code": "ADVENT",
         "name": "MD FARHAN ASIF",
         "mobile": "01841333100",
-        "bank_name": "DUTCH-BANGLA BANK LTD",
-        "branch_name": "MOTIJHEEL FOREIGN EXCHANGE",
-        "account_no": "105.103.15790",
+        "bank_name": "46.5",
+        "branch_name": "55.3",
+        "account_no": "10",
         "created_at": "28th Jan, 2019 11:03:21 PM",
         "updated_at": "24th Jul, 2019 09:39:19 AM",
-        "status": "4",
+        "status": "5",
         "amount": "20,000"
     },
     {
         "id": "5",
         "user_id": "147",
-        "client_code": "2209078",
+        "client_code": "GP",
         "name": "MD FARHAN ASIF",
         "mobile": "01716313161",
-        "bank_name": "DUTCH-BANGLA BANK LTD",
+        "bank_name": "120.5",
         "branch_name": "MOTIJHEEL FOREIGN EXCHANGE",
-        "account_no": "105.103.15790",
+        "account_no": "10",
         "created_at": "3rd Oct, 2018 05:26:08 PM",
         "updated_at": "3rd Oct, 2018 05:26:29 PM",
-        "status": "6",
+        "status": "5",
         "amount": "1,250"
     },
     {
         "id": "1",
         "user_id": "147",
-        "client_code": "2209078",
+        "client_code": "AMANFEED",
         "name": "MD FARHAN ASIF",
         "mobile": "01716313161",
-        "bank_name": "DUTCH-BANGLA BANK LTD",
+        "bank_name": "56.78",
         "branch_name": "MOTIJHEEL FOREIGN EXCHANGE",
-        "account_no": "105.103.15790",
+        "account_no": "10",
         "created_at": "23rd Jul, 2018 04:28:31 PM",
         "updated_at": "13th Aug, 2018 06:47:55 PM",
         "status": "5",
@@ -155,7 +155,7 @@ export default class OrderHistoryComponent extends React.Component {
                     <FlatList
                         data={item_data}
                         renderItem={({ item, key }) => (
-                            <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#104881', '#1CB5E0', ]} style={styles.cardView} key={key}>
+                            <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#0072ff', '#00c6ff', ]} style={styles.cardView} key={key}>
                             
                                 <View style={{padding: 5, flexDirection: 'row', alignItems: 'flex-end', justifyContent:'space-between'}}>
                                     <Text style={{color:'#FFF', marginTop: 5}}>{item.created_at}</Text>
@@ -163,27 +163,27 @@ export default class OrderHistoryComponent extends React.Component {
                                         item.status == 1 ? 
                                         (
                                             <Badge style={{ backgroundColor: 'orange'}}>
-                                                <Text style={{color: 'white', fontWeight: '800'}}>Pending</Text>
+                                                <Text style={{color: 'white', fontWeight: 'bold'}}>Pending</Text>
                                             </Badge>
                                         )
                                         :
                                         (
                                             <Badge style={ item.status == 5 ? { backgroundColor: 'green'} : { backgroundColor: 'red'}}>
-                                                <Text style={{color: 'white', fontWeight: '800'}}>{ item.status == 5 ? 'Executed' : 'Cancelled'}</Text>
+                                                <Text style={{color: 'white', fontWeight: 'bold'}}>{ item.status == 5 ? 'Executed' : 'Cancelled'}</Text>
                                             </Badge>
                                         )
                                     }
                                     
                                 </View>
                                 <View style={{padding: 5, flexDirection: 'row', alignItems: 'flex-end', justifyContent:'space-between'}}>
-                                    <Text style={{fontSize:14, fontWeight:'bold', color:'#FFF'}}>Client Code: {item.client_code}</Text>
+                                    <Text style={{fontSize:14, fontWeight:'bold', color:'#FFF'}}>Security Code: {item.client_code}</Text>
                                     <Text style={{color:'#FFF'}}>{item.name}</Text>
                                 </View>
                                 <View style={{padding: 5, flexDirection: 'row', alignItems: 'flex-end', justifyContent:'space-between'}}>
-                                    <Text style={{color:'#FFF'}}>Bank Name: {item.bank_name}</Text>
+                                    <Text style={{color:'#FFF'}}>Current Rate: {item.bank_name}</Text>
                                 </View>
                                 <View style={{padding: 5, flexDirection: 'row', alignItems: 'flex-end', justifyContent:'space-between'}}>
-                                    <Text style={{color:'#FFF'}}>Account No: {item.account_no}</Text>
+                                    <Text style={{color:'#FFF'}}>Number of Share: {item.account_no}</Text>
                                 </View>
                                 <View style={{padding: 5, flexDirection: 'row', alignItems: 'flex-end', justifyContent:'space-between'}}>
                                     <Text style={{fontSize:14, fontWeight:'bold', color:'#FFF', marginBottom: 5}}>Amount: {item.amount}</Text>
